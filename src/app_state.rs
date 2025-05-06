4eec4eb77a46f0798607f4ed9c396110475e575c
Jesse Abramowitz <jesse@entropy.xyz>
2025-05-06 13:27:34 -0400

    app state framework

---
+++ src/app_state.rs
@@ -0,0 +1,46 @@
+use serde::Deserialize;
+use sp_core::{Pair, crypto::AccountId32, sr25519};
+use std::{
+    collections::HashMap,
+    sync::{Arc, RwLock},
+};
+use x25519_dalek::StaticSecret;
+/// Application state struct which is cloned and made available to every axum HTTP route handler function
+#[derive(Clone)]
+pub struct AppState {
+    /// Keypair for box id account
+    pub pair: sr25519::Pair,
+    /// Secret encryption key
+    pub x25519_secret: StaticSecret,
+    /// Configuation containing the chain endpoint
+    pub configuration: Configuration,
+    /// Storage for api keys
+    pub api_keys: Arc<RwLock<HashMap<AccountId32, String>>>,
+}
+
+impl AppState {
+    /// Setup AppState with given secret keys
+    pub fn new(
+        configuration: Configuration,
+        pair: sr25519::Pair,
+        x25519_secret: StaticSecret,
+    ) -> Self {
+        Self {
+            pair,
+            x25519_secret,
+            configuration,
+            api_keys: Arc::new(RwLock::new(Default::default())),
+        }
+    }
+}
+
+#[derive(Deserialize, Debug, Clone)]
+pub struct Configuration {
+    pub endpoint: String,
+}
+
+impl Configuration {
+    pub fn new(endpoint: String) -> Configuration {
+        Configuration { endpoint }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,47 @@
-use clap::Parser;
+pub mod app_state;
+
 use anyhow::anyhow;
-use std::{net::SocketAddr, str::FromStr};
+use app_state::{AppState, Configuration};
 use axum::Router;
+use clap::Parser;
+use std::{net::SocketAddr, str::FromStr};
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     let args = StartupArgs::parse();
+    let configuration = Configuration::new(args.chain_endpoint);
+    // TODO add keys and app state
+    // TODO add loki maybe
     let addr = SocketAddr::from_str(&args.box_url)
         .map_err(|_| anyhow!("Failed to parse threshold url"))?;
     let listener = tokio::net::TcpListener::bind(&addr)
         .await
         .map_err(|_| anyhow!("Unable to bind to given server address"))?;
-    
+
     let mut routes = Router::new();
     // TODO: add loggings
 
     axum::serve(listener, routes.into_make_service()).await?;
-    Ok(())  
+    Ok(())
 }
-
 
 #[derive(Parser, Debug, Clone)]
 #[command(about, version)]
 pub struct StartupArgs {
-     /// Url to host threshold (axum) server on.
-     #[arg(short = 'u', long = "box-url", required = false, default_value = "127.0.0.1:3001")]
-     pub box_url: String,
+    /// Url to host threshold (axum) server on.
+    #[arg(
+        short = 'u',
+        long = "box-url",
+        required = false,
+        default_value = "127.0.0.1:3001"
+    )]
+    pub box_url: String,
+    /// Websocket endpoint for the entropy blockchain.
+    #[arg(
+        short = 'c',
+        long = "chain-endpoint",
+        required = false,
+        default_value = "ws://localhost:9944"
+    )]
+    pub chain_endpoint: String,
 }

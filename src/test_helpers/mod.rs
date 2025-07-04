a24823712033d079ce3f21875c1356bbe888256f
Jesse Abramowitz <jesse@entropy.xyz>
2025-07-04 14:34:21 -0400

    tests

--- src/test_helpers/mod.rs
+++ src/test_helpers/mod.rs
@@ -3,7 +3,7 @@ mod test_server;
 
 use crate::{
     app,
-    app_state::{AppState, Configuration},
+    app_state::{AppState},
 };
 use entropy_api_key_service_client::ApiKeyServiceClient;
 use rand_core::OsRng;
@@ -11,6 +11,7 @@ use sp_core::{Pair, sr25519};
 use sp_keyring::sr25519::Keyring;
 use test_server::start_test_api_server;
 use x25519_dalek::StaticSecret;
+use entropy_client::forest::{TreeState, Configuration};
 
 pub const DEFAULT_ENDPOINT: &str = "ws://localhost:9944";
 
@@ -20,7 +21,7 @@ pub async fn setup_client() -> AppState {
     let (pair, _seed) = sr25519::Pair::generate();
     let x25519_secret = StaticSecret::random_from_rng(OsRng);
 
-    let app_state = AppState::new(configuration, pair, x25519_secret);
+    let app_state = AppState::new(TreeState::new(configuration, pair, x25519_secret));
     let app = app(app_state.clone()).into_make_service();
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3001")
@@ -40,7 +41,7 @@ pub async fn setup_client() -> AppState {
 pub fn make_test_client(app_state: &AppState, keyring: &Keyring) -> ApiKeyServiceClient {
     ApiKeyServiceClient::new(
         "http://127.0.0.1:3001".to_string(),
-        app_state.x25519_public_key(),
+        app_state.tree_state.x25519_public_key(),
         keyring.pair(),
     )
 }
